905c8f42060a897b6d3a8f1c89515bd62d16507a
Omer Karim <116109574+omer36@users.noreply.github.com>
2022-10-21 13:22:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function gorren(){
+    let wat = document.getElementById('w').value;
+    let volt =  document.getElementById('v').value;
+    re = wat/volt;
+    document.getElementById('result').innerHTML = re + " ئەمپێر";
+}
